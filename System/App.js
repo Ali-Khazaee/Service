0521162279aecd025b6d74b3e1cfea21625a63d6
Ali Khazaee <dev.khazaee@gmail.com>
2018-06-10 05:34:35 +0430

    Start Connecting To Android

--- System/App.js
+++ System/App.js
@@ -3,6 +3,7 @@
 // Environment
 process.env.NODE_ENV = 'production'
 
+const FS = require('fs')
 const Net = require('net')
 
 const Misc = require('./Handler/Misc')
@@ -15,27 +16,19 @@ Server.on('connection', function(Client2)
 {
     const Client = new Socket(Client2)
 
-    Client.on('emit', function(Data)
+    Client.on('SendMessage', function(Data, CallBack)
     {
-        console.log('Server Emit: ' + Data)
+        CallBack('CallBack')
     })
 
-    Client.on('emit2', function(Data, CallBack)
+    Client.on('SendData', function(ReadStream, Data, CallBack)
     {
-        console.log('Server Emit2 Ack: ' + Data)
-        CallBack('Server Result Emit2')
-    })
+        var WriteStream = FS.createWriteStream(Data.name)
+        ReadStream.pipe(WriteStream)
 
-    Client.on('stream', function(readStream, Data, CallBack)
-    {
-        console.log('Server Stream With Ack: ' + Data)
-
-        var writeStream = require('fs').createWriteStream(Data.name)
-        readStream.pipe(writeStream)
-
-        writeStream.on('finish', function()
+        WriteStream.on('finish', function()
         {
-            CallBack('Server Result Stream')
+            CallBack('CallBack')
         })
     })
 })
@@ -54,25 +47,3 @@ Server.listen(Config.SERVER_PORT, '0.0.0.0', function()
 {
     Misc.Analyze('ServerOnListen', { })
 })
-
-/*
-*
-*
-*/
-
-const Socket2 = require('fast-tcp').Socket
-const socket = new Socket2({ host: 'localhost', port: Config.SERVER_PORT })
-
-socket.emit('emit', 'Data e Emit')
-
-socket.emit('emit2', 'Data e Emit2', function(response)
-{
-    console.log('Client Emit2 Ack: ' + response)
-})
-
-var writeStream = socket.stream('stream', { name: 'img-copy.jpg' }, function(response)
-{
-    console.log('Client Stream Ack: ' + response)
-})
-
-require('fs').createReadStream('./System/img.jpg').pipe(writeStream)
