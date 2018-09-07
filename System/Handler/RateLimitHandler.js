c987ce699ba57578b42d7370971adab6c73c0e85
Ali Khazaee <dev.khazaee@gmail.com>
2018-09-07 06:31:30 +0430

    RateLimitHandler Performance

--- System/Handler/RateLimitHandler.js
+++ System/Handler/RateLimitHandler.js
@@ -14,43 +14,33 @@ module.exports = (Count, Time) =>
 
         if (RateLimitList.has(Key))
         {
-            const Value = RateLimitList.get(Key)
-
-            return
-        }
-
-        RateLimitList.set(Key, new Map([ [ Message[Type.Packet], { Count: 3, Time: 10 } ] ]))
-        Next()
-
-        DB.collection('ratelimit').find({ Key: Key }).limit(1).project({ _id: 1, Time: 1, Count: 1 }).toArray((Error, Result) =>
-        {
-            if (Misc.IsDefined(Error))
+            if (RateLimitList.get(Key).has(Message[Type.Packet]))
             {
-                Misc.Analyze('DBError', { Tag: 'RateLimit', Error: Error })
-                Message[Type.Client].Send(Message[Type.Packet], Message[Type.ID], { Result: -1 })
+                const Value = RateLimitList.get(Key).get(Message[Type.Packet])
+
+                if (Value.Expire < TimeCurrent)
+                {
+                    Value.Count = 1
+                    Value.Expire = TimeCurrent + Time
+                    return Next()
+                }
+
+                if (Value.Count <= Count)
+                {
+                    Value.Count += 1
+                    return Next()
+                }
+
+                Message[Type.Client].Send(Message[Type.Packet], Message[Type.ID], { Result: -2 })
                 return
             }
 
-            if (Misc.IsUndefined(Result[0]))
-            {
-                DB.collection('ratelimit').insertOne({ Key: Key, Count: 1, Expire: TimeCurrent + Time })
-                return Next()
-            }
+            RateLimitList.get(Key).set(Message[Type.Packet], { Count: 1, Expire: TimeCurrent + Time })
+            return Next()
+        }
 
-            if (Result[0].Expire < TimeCurrent)
-            {
-                DB.collection('ratelimit').updateOne({ _id: Result[0]._id }, { $set: { Count: 1, Expire: TimeCurrent + Time } })
-                return Next()
-            }
-
-            if (Result[0].Count <= Count)
-            {
-                DB.collection('ratelimit').updateOne({ _id: Result[0]._id }, { $set: { Count: Result[0].Count + 1 } })
-                return Next()
-            }
-
-            Message[Type.Client].Send(Message[Type.Packet], Message[Type.ID], { Result: -2 })
-        })
+        RateLimitList.set(Key, new Map([ [ Message[Type.Packet], { Count: 1, Expire: TimeCurrent + Time } ] ]))
+        Next()
     }
 }
 
@@ -61,33 +51,5 @@ module.exports.Save = (Count, Time) =>
 
 module.exports.Load = (Count, Time) =>
 {
-    DB.collection('ratelimit').find({ Key: Key }).limit(1).project({ _id: 1, Time: 1, Count: 1 }).toArray((Error, Result) =>
-    {
-        if (Misc.IsDefined(Error))
-        {
-            Misc.Analyze('DBError', { Tag: 'RateLimit', Error: Error })
-            Message[EventHandler.Client].Send(Message[EventHandler.Packet], Message[EventHandler.ID], { Result: -1 })
-            return
-        }
 
-        if (Misc.IsUndefined(Result[0]))
-        {
-            DB.collection('ratelimit').insertOne({ Key: Key, Count: 1, Expire: TimeCurrent + Time })
-            return Next()
-        }
-
-        if (Result[0].Expire < TimeCurrent)
-        {
-            DB.collection('ratelimit').updateOne({ _id: Result[0]._id }, { $set: { Count: 1, Expire: TimeCurrent + Time } })
-            return Next()
-        }
-
-        if (Result[0].Count <= Count)
-        {
-            DB.collection('ratelimit').updateOne({ _id: Result[0]._id }, { $set: { Count: Result[0].Count + 1 } })
-            return Next()
-        }
-
-        Message[EventHandler.Client].Send(Message[EventHandler.Packet], Message[EventHandler.ID], { Result: -2 })
-    })
 }
